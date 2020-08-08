7306ce0be5e7737b7e1867adbd01bbb052899c84
Arunvel Sriram <arunvelsriram@gmail.com>
2020-08-08 15:19:36 +0530

    Add metrics handler

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/gorilla/handlers v1.4.2
+	github.com/prometheus/client_golang v0.9.3
 	github.com/spf13/cobra v1.0.0
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/spf13/viper v1.4.0
--- pkg/server/server.go
+++ pkg/server/server.go
@@ -7,22 +7,32 @@ import (
 
 	"github.com/arunvelsriram/sftp-exporter/pkg/config"
 	"github.com/gorilla/handlers"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 func Start(cfg config.Config) error {
 	r := http.NewServeMux()
 
-	r.Handle("/healthz", withLogging(healthzHandler))
+	r.Handle("/healthz", healthzHandler())
+	r.Handle("/metrics", metricsHandler())
 
 	addr := fmt.Sprintf("%s:%d", cfg.GetBindAddress(), cfg.GetPort())
 	fmt.Printf("Listening on: %s\n", addr)
 	return http.ListenAndServe(addr, r)
 }
 
-func withLogging(h http.HandlerFunc) http.Handler {
+func withLogging(h http.Handler) http.Handler {
 	return handlers.LoggingHandler(os.Stdout, h)
 }
 
-func healthzHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "healthy")
+func healthzHandler() http.Handler {
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "healthy")
+	})
+	return withLogging(handler)
+}
+
+func metricsHandler() http.Handler {
+	handler := prometheus.Handler()
+	return withLogging(handler)
 }
